3e1a8547f738cd9013a1fecdc0ba78ba58015dd9
YogaVicky <yogasrivarshan@gmail.com>
2019-06-25 23:09:58 +0530

    update

--- Lists/basicList.c
+++ Lists/basicList.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
+struct Node;
+typedef struct Node* Ptr;
+typedef Ptr list;
+typedef Ptr pos;
+
+void insert(int a , list l , pos p){
+	pos tmp;
+	temp = malloc(sizeof(struct Node));
+	if(tmp == NULL)
+		FatalError("Out of space");
+	tmp->a = a;
+	tmp->next = p->next;
+	p->next = tmp;
+}
+
 struct Node{
 	int a;
-	int next;
+	pos next;
+}
+
+int main(){
+	return 0;
 }
