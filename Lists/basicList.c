c3d250fedb1d762b23a7ce8b6ca9797467809208
YogaVicky <yogasrivarshan@gmail.com>
2019-08-17 23:23:00 +0530

    update

--- Lists/basicList.c
+++ Lists/basicList.c
@@ -2,12 +2,41 @@
 
 struct node{
 	int data;
-	int key;
 	struct node *next;
 };
 
 struct node *head = NULL;
-struct node *current = NULL;
+
+void insert(int data , int key);
+
+void printlist(){
+	struct node *test = head;
+	while(test!=NULL){
+		printf("%d",test->data);
+	}
+}
+
+int main(){
+	int a;
+	printf("What do you want to do?\n");
+	printf("1.Print the list\n2.Insert\n3.Delete\n4.Check empty\n");
+	scanf("%d",&a);
+	switch(){
+		case 1:
+		break;
+		case 2:
+		break;
+		case 3:
+		break;
+		case 4:
+		break;
+		case 5:
+		break;
+		default:
+			printf("Enter the correct option\n");
+	}
+	return 0;
+}
 
 void insert(int data , int key){
 	if(head == NULL){
@@ -24,24 +53,4 @@ void insert(int data , int key){
 		current->data  = data;
 		current->key = NULL;
 	}
-}
-
-void printlist(){
-	struct node *test = head;
-	while(test!=NULL){
-		printf("%d",test->data);
-	}
-}
-
-int main(){
-	int a;
-	cout<<"Enter the number to be added/n";
-	cin>>a;
-	insert(a,&a);
-	cout<<"Enter the number to be added/n";
-	cin>>a;
-	insert(a,&a);
-	printf("\n");
-	printlist();
-	return 0;
 }
